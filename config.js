13f6a7742c37e893d7fbe643f4533f03d44cea92
FREDDY-QUEZADA <134630066+FREDDY-QUEZADA@users.noreply.github.com>
2023-10-19 19:27:30 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    PORT: 3000, 
+    DB_URL: 'mongodb+srv://freddy:freddyquezada@cluster0.jry94f4.mongodb.net/?retryWrites=true&w=majority'
+}
+
+module.exports = config
---
+++ network/routes.js
@@ -0,0 +1,11 @@
+
+const empresa = require('../components/empresa/interface')
+const representante_legal = require('../components/representante_legal/interface')
+
+const routes = function(server) {
+   
+    server.use('/empresa', empresa)
+    server.use('/representante_legal', representante_legal)
+}
+
+module.exports = routes
